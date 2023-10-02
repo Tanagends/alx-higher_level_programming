a89ac620973194be36a9b70366bdc24d2d74295e
Tanagends <tanagends@gmail.com>
2023-10-02 15:13:52 -0700

    hello

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-str = str.split()
+ language that combines remarkable power with very clear syntax".split()
 str = str[5] + " " + str[6] + " " + str[12] + " " + str[0]
 print(str)
