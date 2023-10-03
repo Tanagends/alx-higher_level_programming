209a7496f425b9e9ddc4559181049e49ef9cbc6d
Tanagends <tanagends@gmail.com>
2023-10-03 14:22:03 -0700

    11-pow.py

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,8 @@
+#!user/bin/python3
+def print_last_digit(number):
+    if number < 0:
+        last = ((number * -1) % 10) * -1
+    else:
+        last = number % 10
+    print("{0}".format(last), end="")
+    return last
