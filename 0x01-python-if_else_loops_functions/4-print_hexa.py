1a56b41767bf6ed562895ec7221a7c0fe952630a
Tanagends <tanagends@gmail.com>
2023-10-03 14:33:08 +0200

    loops

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for i in range(99):
-    print(i, "=", hex(i))
+    print("{0} {i} {2}".format(i, "=", hex(i)))
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,5 +3,5 @@ for i in range(100):
     if i < 10:
         print(0, i, sep="", end=", ")
     else:
-        print(i, end=", " if i != 99 else "\n")
+        print("{0}".format(i), end=", " if i != 99 else "\n")
 
