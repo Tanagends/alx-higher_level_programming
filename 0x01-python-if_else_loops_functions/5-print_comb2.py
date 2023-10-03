de7b52ed955add42f293c134061cff335a42f11f
Tanagends <tanagends@gmail.com>
2023-10-03 14:40:52 +0200

    loops

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(100):
     if i < 10:
-        print(0, i, sep="", end=", ")
+        print("{0}{1}".format(0, i), end=", ")
     else:
         print("{0}".format(i), end=", " if i != 99 else "\n")
 
