7e5c21a62b16e918b6c741e0b3ff20598fee9b06
Tanagends <tanagends@gmail.com>
2023-10-03 14:58:22 +0200

    loops

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for i in range(10):
+    for j in range(10):
+        if i < j:
+            print("{0}{1}".format(i, j), end="\n" \
+                    if i == 8 and j == 9 else ", ")
