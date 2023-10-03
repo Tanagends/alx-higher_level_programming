93e2c494844fe63c28a41f21fa7c7fb867186dcb
Tanagends <tanagends@gmail.com>
2023-10-03 15:10:18 -0700

    102-magic_calculation.py

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * insert_node - Inserts a number into a sorted list.
+ * @head: A pointer the head of the list
+ * @number: The number
+ * Return: NULL If the function fails, OTHERWISE A POINTER
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *node = *head, *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
+
+	if (node == NULL || node->n >= number)
+	{
+		new->next = node;
+		*head = new;
+		return (new);
+	}
+
+	while (node && node->next && node->next->n < number)
+		node = node->next;
+
+	new->next = node->next;
+	node->next = new;
+
+	return (new);
+}
