61c8d798d53fc5fefe341538f4008c1f8626012d
Tanagends <tanagends@gmail.com>
2023-10-05 18:29:15 +0200

    modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,9 +4,8 @@ import hidden_4
 
 
 if __name__ == "__main__":
-    
     li = dir(hidden_4)
     li.sort()
     for element in li:
         if not element.startswith("_"):
-            print(element)
+            print(ielement)
