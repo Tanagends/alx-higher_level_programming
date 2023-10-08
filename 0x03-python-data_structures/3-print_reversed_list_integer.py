fef3654a372c161366cde5270e0478b9d2150418
Tanagends <tanagends@gmail.com>
2023-10-08 23:04:46 +0200

    lists and tuples

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if len(my_list) != 0:
+    if my_list:
         list_copy = my_list.copy()
         list_copy.reverse()
         for integer in list_copy:
