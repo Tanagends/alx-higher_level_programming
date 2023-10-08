a10691c11ce6227b3c7e2160ad5ec5ba8267829e
Tanagends <tanagends@gmail.com>
2023-10-08 20:17:02 +0200

    lists and tuples

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome.
+ * @head: pointer to the head pointer
+ *
+ * Return: 1 (If palindrome 0 Otherwise)
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t copy;
+	int count = 0, half, i, *arr;
+
+	if (head == NULL || *head == NULL || *head->next == NULL)
+		return (1);
+	copy = *head;
+	for (count = 0; copy != NULL; count++)
+		copy = copy->next;
+	copy = *head;
+	half = count / 2;
+	for (i = 0; i < half; i++)
+		copy = copy->next;
+	if (count % 2 == 1)
+		copy = copy->next;
+	arr = malloc(sizeof(int) * half);
+	if (!arr)
+		exit(1);
+	for (i = half - 1; i >= 0; i--)
+	{
+		arr[i] = copy->n;
+		copy = copy->next;
+	}
+	copy = *head;
+	for (i = 0; i < half; i++)
+	{
+		if (copy->n != arr[i])
+		{
+			free(arr);
+			return (0);
+		}
+		copy = copy->next;
+	}
+	free(arr);
+	return (1);
+}
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    if len(my_list) == 0:
+        return
     list_copy = my_list.copy()
     list_copy.reverse()
     for integer in list_copy:
         print("{:d}".format(integer))
-print_reversed_list_integer([1, 2, 3])
