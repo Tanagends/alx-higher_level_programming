b39cd90b6c8c2bd6d49614ca61f7de54ab62f33c
Tanagends <tanagends@gmail.com>
2023-10-11 23:49:46 +0200

    dictionaries and sets

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    diction = dict(filter(lambda dictionary: dictionary[1] != value, a_dictionary.items()))
+    diction = dict(filter(lambda dictionary: dictionary[1] != value,
+                   a_dictionary.items()))
     a_dictionary.clear()
     a_dictionary.update(diction)
     return a_dictionary
