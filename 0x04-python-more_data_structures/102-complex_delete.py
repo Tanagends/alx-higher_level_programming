746fbec536fc7f58bfcee76d9389d5065d85de1e
Tanagends <tanagends@gmail.com>
2023-10-11 23:46:51 +0200

    dictionaries and sets

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -4,15 +4,3 @@ def complex_delete(a_dictionary, value):
     a_dictionary.clear()
     a_dictionary.update(diction)
     return a_dictionary
-a_dictionary = {'lang': "C", 'track': "Low", 'pref': "C", 'ids': [1, 2, 3]}
-new_dict = complex_delete(a_dictionary, 'C')
-print(a_dictionary)
-print("--")
-print(new_dict)
-
-print("--")
-print("--")
-new_dict = complex_delete(a_dictionary, 'c_is_fun')
-print(a_dictionary)
-print("--")
-print(new_dict)
