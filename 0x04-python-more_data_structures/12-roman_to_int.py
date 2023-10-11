4e536c021ee3482eb5f96f1c9de6aefc6733f945
Tanagends <tanagends@gmail.com>
2023-10-11 21:15:05 +0200

    dictionaries and sets

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     roman_list = list(roman.keys())
     val_list = []
-    if not roman_string:
+    if not roman_string or roman_string not str:
         return 0
     for char in roman_string:
         if char not in roman_list:
