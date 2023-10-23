cda4d63f790bdfc9f859fb71ce1333a9aa9bd713
Tanagends <tanagends@gmail.com>
2023-10-23 21:46:04 +0200

    exceptions

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -8,6 +8,6 @@ def magic_calculation(a, b):
             else:
                 result += (a ** b) / i
         except Exception:
-                result = b + a
-                break
+            result = b + a
+            break
     return result
