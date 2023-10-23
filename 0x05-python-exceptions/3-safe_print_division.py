4b34dfe6f2faff278624e3ab37158d2829c525ce
Tanagends <tanagends@gmail.com>
2023-10-23 14:13:20 +0200

    exceptions

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        pass
+    finally:
+        try:
+            print("Inside result: {}".format(result))
+            return result
+        except NameError:
+            return None
