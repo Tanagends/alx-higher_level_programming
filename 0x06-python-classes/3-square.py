dd879680202e4ba120f7101ba67aa59e6d9acfc2
Tanagends <tanagends@gmail.com>
2023-10-24 20:43:22 +0200

    classes

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -19,3 +19,7 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
+    def area(self):
+        """Computes square area"""
+        return self.__size ** 2 
