738df74b973b245c700c54186d6bb5648d7e0d6c
Tanagends <tanagends@gmail.com>
2023-10-24 20:11:18 +0200

    classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """Represents a square
+
+    Attributes:
+        __size: square size
+    """
+
+    def __init__(self, size):
+        """Instantiates the square class
+
+        Args: 
+            size: the size of the square defined
+        """
+        self.__size = size
