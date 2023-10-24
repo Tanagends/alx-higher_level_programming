0f05137cbe794cbba0b8cefcfbf8d30fe27fc1f9
Tanagends <tanagends@gmail.com>
2023-10-24 21:05:28 +0200

    classes

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -20,11 +20,6 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-
-    def area(self):
-        """Computes square area"""
-        return self.__size ** 2
-
     @property
     def size(self):
         """Retrives a square size"""
@@ -37,3 +32,7 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
+    def area(self):
+        """Computes square area"""
+        return self.__size ** 2
