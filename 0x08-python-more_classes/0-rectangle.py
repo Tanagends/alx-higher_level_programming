e90a4f4e15637c774dc10032e1bff191b3520698
Tanagends <tanagends@gmail.com>
2023-10-30 15:23:23 +0200

    objects and classes

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""My Rectangle Module"""
+
+class Rectangle:
+    """Defines a rectangle and its operations"""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+More tasks on Classes and Objects in Python.
