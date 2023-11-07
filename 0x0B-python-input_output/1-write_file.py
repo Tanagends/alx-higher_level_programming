e0b8eef3a1474ac980f7467cfbc999d91ebb6bf1
Tanagends <tanagends@gmail.com>
2023-11-07 21:46:26 +0200

    file i/o

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module for writing string to file"""
+
+
+def write_file(filename="", text=""):
+    """"writes to a file"""
+    with open(filename, 'w', encoding="utf-8") as file:
+        file.write(text)
