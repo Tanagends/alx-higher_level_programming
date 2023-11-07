ae31a18ac6f8491ed60b3395cd601599a4284f1c
Tanagends <tanagends@gmail.com>
2023-11-07 22:03:26 +0200

    file i/o

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,4 +5,4 @@
 def write_file(filename="", text=""):
     """"writes to a file"""
     with open(filename, 'w', encoding="utf-8") as file:
-        file.write(text)
+        return file.write(text)
