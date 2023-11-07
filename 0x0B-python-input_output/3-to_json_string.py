2506b8fe7bad4583668676839903e2ef7b3f4111
Tanagends <tanagends@gmail.com>
2023-11-07 22:55:28 +0200

    file i/o

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import json
 """My Serialization Module"""
+import json
 
 
 def to_json_string(my_obj):
     """Serializes object"""
-    return json.dumps(my_obj)
+    return str(json.dumps(my_obj))
--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import json
 """My Deserialization Module"""
+import json
 
 
 def to_json_string(my_obj):
