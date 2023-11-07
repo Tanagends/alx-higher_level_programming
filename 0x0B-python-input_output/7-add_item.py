6c6204a0906708d19c3e692c1d17a49fceec0c22
Tanagends <tanagends@gmail.com>
2023-11-07 23:55:45 +0200

    file i/o

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,4 +6,5 @@ save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 file_list = load_from_json_file(add_item.json)
-save_to_json_file(file_list.extend(sys.argv[1:]), add_item.json)
+file_list.extend(sys.argv[1:])
+save_to_json_file(file_list, add_item.json)
