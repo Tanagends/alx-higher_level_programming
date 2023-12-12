ca6876eacae54e2aeb27ddce6b0ad4f909b7c284
Tanagends <tanagends@gmail.com>
2023-12-13 01:27:28 +0200

    sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,6 +1,6 @@
 --lists all records of the table second_table with name
 --Records are ordered by descending score
-SELECT score, name
-FROM second_table
-WHERE name != ""
-ORDER BY score DESC;
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `name` != ""
+ORDER BY `score` DESC;
