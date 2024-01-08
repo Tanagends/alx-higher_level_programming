ff96acdd07f2469d8dec9b48090681adf91cb857
Tanagends <tanagends@gmail.com>
2024-01-09 00:34:37 +0200

    javascript

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 const myObject = {
-	  type: 'object',
-	  value: 12
+  type: 'object',
+  value: 12
 };
 console.log(myObject);
 myObject.incr = function () {
   (myObject.value)++;
-}
+};
 myObject.incr();
 console.log(myObject);
 myObject.incr();
