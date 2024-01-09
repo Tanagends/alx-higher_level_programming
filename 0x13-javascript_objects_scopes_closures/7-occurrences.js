b8fab7c5e5bee383428f3ad07fd982c919d01eb4
Tanagends <tanagends@gmail.com>
2024-01-09 16:10:45 +0200

    javascript oop

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,1 +1,11 @@
-`
+#!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  for (let element of list) {
+    if (element === searchElement) {
+      count++;
+    }
+  }
+  return (count);
+}
