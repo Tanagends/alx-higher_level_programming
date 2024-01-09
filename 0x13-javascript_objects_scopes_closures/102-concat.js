f2b01fea0fd8f8a479ede3adbaada9ff2fe28010
Tanagends <tanagends@gmail.com>
2024-01-10 01:44:17 +0200

    javascript oop

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,17 +2,6 @@
 
 const fs = require('fs');
 
-const fileA = process.argv[2];
-const fileB = process.argv[3];
-const fileC = process.argv[4];
-
-fs.readFile(fileA, 'utf8', (err, dataA) => {
-  if (err) throw err;
-  fs.readFile(fileB, 'utf8', (err, dataB) => {
-    if (err) throw err;
-    const concatenatedData = dataA + dataB;
-    fs.writeFile(fileC, concatenatedData, (err) => {
-      if (err) throw err;
-    });
-  });
-});
+const fArg = fs.readFileSync(process.argv[2]).toString();
+const sArg = fs.readFileSync(process.argv[3]).toString();
+fs.writeFileSync(process.argv[4], fArg + sArg);i
