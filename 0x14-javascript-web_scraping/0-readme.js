cb624837e8b78cc2bef616851cfaa2e756107a90
Tanagends <tanagends@gmail.com>
2024-02-21 01:43:37 +0200

    read a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,17 +1,15 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-
-if (process.argv.length != 3) {
-  console.log("Pass in one file argument");
+if (process.argv.length !== 3) {
+  console.log('Pass in one file argument');
   process.exit(1);
 }
 
 fs.readFile(process.argv[2], 'utf-8', (error, content) => {
   if (error) {
     console.error(error);
-  }
-  else {
+  } else {
     console.log(content);
-}
+  }
 });
