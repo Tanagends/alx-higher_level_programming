fc128999f6a016698d516c75419b3468c641649d
Tanagends <tanagends@gmail.com>
2024-02-21 01:38:52 +0200

    read a file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+
+if (process.argv.length != 3) {
+  console.log("Pass in one file argument");
+  process.exit(1);
+}
+
+fs.readFile(process.argv[2], 'utf-8', (error, content) => {
+  if (error) {
+    console.error(error);
+  }
+  else {
+    console.log(content);
+}
+});
---
+++ 0x14-javascript-web_scraping/README.md
@@ -0,0 +1,1 @@
+JS webscraping tasks
